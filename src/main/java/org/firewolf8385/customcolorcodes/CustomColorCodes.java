004f4871fca3e26e8f3782dd2a8f4a6ed35aac0b
Colby <colbykrawczyk10@gmail.com>
2020-04-22 16:50:10 -0400

    Created Settings Manager.

--- src/main/java/org/firewolf8385/customcolorcodes/CustomColorCodes.java
+++ src/main/java/org/firewolf8385/customcolorcodes/CustomColorCodes.java
@@ -4,11 +4,12 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class CustomColorCodes extends JavaPlugin
 {
+    Settings settings = Settings.getInstance();
 
     @Override
     public void onEnable()
     {
-
+        settings.setup(this);
     }
 
 }
