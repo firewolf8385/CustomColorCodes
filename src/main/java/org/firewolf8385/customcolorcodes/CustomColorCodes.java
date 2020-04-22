06122d41ec8a02347a55d278bed392098d4fdf67
Colby <colbykrawczyk10@gmail.com>
2020-04-22 16:30:27 -0400

    Add initial files.

---
+++ src/main/java/org/firewolf8385/customcolorcodes/CustomColorCodes.java
@@ -0,0 +1,14 @@
+package org.firewolf8385.customcolorcodes;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CustomColorCodes extends JavaPlugin
+{
+
+    @Override
+    public void onEnable()
+    {
+
+    }
+
+}
